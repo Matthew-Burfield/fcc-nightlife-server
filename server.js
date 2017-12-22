e1e647fd831f2e15e650a7f99158191c43bbbeff
Matthew-Burfield <m1.burfield@gmail.com>
2017-12-22 17:18:14 +1000

    added API to get data from YELP api

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 
+axios.defaults.baseURL = 'https://api.yelp.com'
+axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.YELP_API_KEY}`
+axios.defaults.headers.common['Accept-Language'] = 'en_US'
+axios.defaults.headers.post['Content-Type'] = 'application/graphql'
+
 app.get('/', (req, res) => res.send('Hello world!'))
+
+app.get('/getRestaurants', (req, res) => {
+	axios.post('/v3/graphql',`{
+			search(location: "brisbane australia") {
+				total
+				business {
+						name
+						id
+						rating
+						photos
+					}
+			}
+	}`)
+	.then((response) => {
+		if (response && response.data) {
+			res.json(response.data)
+		} else {
+			res.sendStatus(500)
+		}
+	})
+})
 
 const port = process.env.PORT || 8000
 
