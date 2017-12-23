a8db936fb303bea3736447f890989b6b06d57b27
Matthew-Burfield <m1.burfield@gmail.com>
2017-12-23 17:58:31 +1000

    updated /getRestaurant API - is now a POST. Sending the location string. Adds the count

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const axios = require('axios')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
 const mongodb = require('mongodb')
-
-const app = express()
 
 const corsOptions = {
   origin: 'http://burfield-nightlife.surge.sh',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
+const mongoUri = process.env.MONGO_CONNECTION
+
+const app = express()
+app.use(bodyParser.json())
 app.use(cors(corsOptions))
 
-const mongoUri = process.env.MONGO_CONNECTION
 const connectToDatabase = (res, callback) => {
 	const response = (db, err, responseValues) => {
 		db.close()
@@ -34,9 +36,10 @@ axios.defaults.headers.post['Content-Type'] = 'application/graphql'
 
 app.get('/', (req, res) => res.send('Hello world!'))
 
-app.get('/getRestaurants', (req, res) => {
+app.post('/restaurants', (req, res) => {
+	const location = req.body.location
 	axios.post('/v3/graphql',`{
-			search(location: "brisbane australia") {
+			search(location: "${location}") {
 				total
 				business {
 						name
@@ -48,19 +51,26 @@ app.get('/getRestaurants', (req, res) => {
 	}`)
 	.then((response) => {
 		if (response && response.data) {
-			const listOfRestaurantIds = response.data.data.search.business.map(biz => biz.id)
+			const restaurantList = response.data.data.search.business.reduce((obj, item) => {
+				obj[item.id] = Object.assign({}, item, { count: 0 })
+				return obj
+			}, {})
+			const restaurantIds = Object.keys(restaurantList)
 			connectToDatabase(res, (db) => {
 				db.collection('restaurant')
 				.aggregate([ 
 					{
-						$match: { id: { $in: listOfRestaurantIds } },
+						$match: { id: { $in: restaurantIds } },
 					},
 					{
 						$group: { _id: "$id", count: { $sum: 1 } }
 					},
 				])
 				.toArray(function(err, result) {
-					res.json(result)
+					result.forEach(restaurant => {
+						restaurantList[restaurant._id].count = restaurant.count
+					})
+					res.json(restaurantList)
 					db.close()
 				})
 			})
